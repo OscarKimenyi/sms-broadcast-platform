df9d826bd1ec979d2302ee9e73bd95c215d697d6
Oscar F. Kimenyi <oscarollie366@gmail.com>
2026-05-06 10:15:15 +0300

    Add duplicate route

--- backend/src/routes/campaign.routes.js
+++ backend/src/routes/campaign.routes.js
@@ -3,14 +3,15 @@ const auth = require('../middleware/auth');
 const {
     getCampaigns, getCampaign, createCampaign,
     sendCampaign, getCampaignLogs, deleteCampaign,
+    duplicateCampaign,
 } = require('../controllers/campaign.controller');
 
 router.use(auth);
-
 router.get('/', getCampaigns);
 router.get('/:id', getCampaign);
 router.post('/', createCampaign);
 router.post('/:id/send', sendCampaign);
+router.post('/:id/duplicate', duplicateCampaign);
 router.get('/:id/logs', getCampaignLogs);
 router.delete('/:id', deleteCampaign);
 
